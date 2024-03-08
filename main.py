832d2dfb4b5b63711000d43ccb66658f12645d64
FroostySnoowman <jacob.j.beal@icloud.com>
2024-03-08 11:19:04 -0700

    Updated Main - Fixed Lots Of Issues

--- main.py
+++ main.py
@@ -10,35 +10,26 @@ from datetime import datetime, timedelta
 from ruamel.yaml import YAML
 from gsheets import Sheets
 
-with open(f'{pathlib.Path(__file__).parent.absolute()}/config.yml', 'r') as file:
-    data = yaml.safe_load(file)
+# Function to load configuration from the YAML file
+def load_config():
+    with open(f'{pathlib.Path(__file__).parent.absolute()}/config.yml', 'r') as file:
+        data = yaml.safe_load(file)
+    return data
 
-file_name = data["General"]["FILE_NAME"]
-google_client_secret_file_name = data["Google"]["GOOGLE_CLIENT_SECRET_FILE_NAME"]
-google_spreadsheet_id = data["Google"]["GOOGLE_SPREADSHEET_ID"]
-app_key = data["Dropbox"]["APP_KEY"]
-app_secret = data["Dropbox"]["APP_SECRET"]
-dropbox_acces_token = data["Dropbox"]["DROPBOX_ACCESS_TOKEN"]
-dropbox_file_path = data["Dropbox"]["DROPBOX_FILE_PATH"]
+config = load_config()
 
 async def export_spreadsheet():
-    sheets = Sheets.from_files(f'{pathlib.Path(__file__).parent.absolute()}/{google_client_secret_file_name}')
-
-    spreadsheet = sheets[google_spreadsheet_id]
-
+    sheets = Sheets.from_files(f'{pathlib.Path(__file__).parent.absolute()}/{config["Google"]["GOOGLE_CLIENT_SECRET_FILE_NAME"]}')
+    spreadsheet = sheets[config["Google"]["GOOGLE_SPREADSHEET_ID"]]
     sheet = spreadsheet.sheets[0]
-
     df = sheet.to_frame()
-
-    df.to_csv(f'{pathlib.Path(__file__).parent.absolute()}/{file_name}', sep='\t', index=False)
+    df.to_csv(f'{pathlib.Path(__file__).parent.absolute()}/{config["General"]["FILE_NAME"]}', sep='\t', index=False)
 
 async def upload_txt():
-    dbx_token = dropbox_acces_token
+    dbx_token = config["Dropbox"]["DROPBOX_ACCESS_TOKEN"]
     dbx = dropbox.Dropbox(dbx_token)
-
-    with open(f'{pathlib.Path(__file__).parent.absolute()}/{file_name}', "rb") as f:
-        dbx.files_upload(f.read(), dropbox_file_path, mode=dropbox.files.WriteMode('overwrite'))
-    
+    with open(f'{pathlib.Path(__file__).parent.absolute()}/{config["General"]["FILE_NAME"]}', "rb") as f:
+        dbx.files_upload(f.read(), config["Dropbox"]["DROPBOX_FILE_PATH"], mode=dropbox.files.WriteMode('overwrite'))
     print(f"Successfully uploaded the file to Dropbox at {datetime.now()}")
 
 async def main():
@@ -46,6 +37,7 @@ async def main():
     await upload_txt()
 
 async def run_every_hour():
+    global config
     try:
         await main()
         while True:
@@ -56,40 +48,44 @@ async def run_every_hour():
             await asyncio.sleep(seconds_until_next_run)
             await main()
     except dropbox.exceptions.AuthError:
-        url = f'https://www.dropbox.com/oauth2/authorize?client_id={app_key}&' \
+        url = f'https://www.dropbox.com/oauth2/authorize?client_id={config["Dropbox"]["APP_KEY"]}&' \
             f'response_type=code&token_access_type=offline'
         webbrowser.open(url)
 
-        ACCESS_CODE_GENERATED = input("Enter access token: ")
+        access_code_generated = input("Enter access token: ")
 
-        BASIC_AUTH = base64.b64encode(f'{app_key}:{app_secret}'.encode())
+        basic_auth = base64.b64encode(f'{config["Dropbox"]["APP_KEY"]}:{config["Dropbox"]["APP_SECRET"]}'.encode()).decode()
 
         headers = {
-            'Authorization': f"Basic {BASIC_AUTH}",
+            'Authorization': f"Basic {basic_auth}",
             'Content-Type': 'application/x-www-form-urlencoded',
         }
 
-        data = f'code={ACCESS_CODE_GENERATED}&grant_type=authorization_code'
+        data = f'code={access_code_generated}&grant_type=authorization_code'
 
         response = requests.post('https://api.dropboxapi.com/oauth2/token',
                                 data=data,
-                                auth=(app_key, app_secret))
+                                headers=headers)
 
         new_access_token = json.loads(response.text)['access_token']
 
-        yaml = YAML()
-        yaml.preserve_quotes = True
-        yaml.indent(mapping=4, sequence=4, offset=2)
-        with open('config.yml', 'r') as file:
-            new_data = yaml.load(file)
+        new_yaml = YAML()
+        new_yaml.preserve_quotes = True
+        new_yaml.indent(mapping=4, sequence=4, offset=2)
+        with open(f'{pathlib.Path(__file__).parent.absolute()}/config.yml', 'r') as file:
+            new_data = new_yaml.load(file)
         new_data['Dropbox']['DROPBOX_ACCESS_TOKEN'] = new_access_token
 
-        with open('config.yml', 'w') as file:
-            yaml.dump(new_data, file)
-
-        await asyncio.sleep(30)
+        with open(f'{pathlib.Path(__file__).parent.absolute()}/config.yml', 'w') as file:
+            new_yaml.dump(new_data, file)
         
+        config = load_config()
+
         await main()
+
+if __name__ == "__main__":
+    print("Script has started running!")
+    asyncio.run(run_every_hour())
 
 if __name__ == "__main__":
     print("Script has started running!")
